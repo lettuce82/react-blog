b68561b4cc336baa3ac1569179aba8e969903fc6
ejddi1805 <82057874+ejddi1805@users.noreply.github.com>
2024-09-30 13:51:43 +0900

    Feat:  "상세보기 구현" 모달 창 구현

--- src/App.js
+++ src/App.js
@@ -9,28 +9,52 @@ function App() {
   let post = '강남 우동 맛집';
   let [글제목, 제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
   let [따봉, 따봉변경]= useState(0);
-
-  const 제목수정 = (index) => {
-    const 새글제목 = [...글제목];
-    새글제목[index] = '여자 코트 추천';
-    제목변경(새글제목);
-  }
+  let [modal, setModal] = useState(false);
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4 style={ {color : 'red', fontSize : '16px'} }>ReactBlog</h4>
-        
+        <button onClick={() => {
+
+          const 정렬된제목 = [...글제목].sort((a, b) => a.localeCompare(b));
+          제목변경(정렬된제목);
+
+        }}>가나다순 정렬</button>
       </div>
+
       {글제목.map((제목, index) => (
         <div className='list' key={index}>
-          <h4>{제목} <span onClick={() => 제목수정(index)}>글변경</span>
-          <span onClick={ () => { 따봉변경( 따봉 + 1 ) } }>👍</span> { 따봉 } </h4>
+          <h4 onClick={()=> {setModal(!modal)}}>{제목}
+            <span onClick={() => {
+            const 새글제목 = [...글제목];
+            새글제목[index] = '여자 코트 추천';
+            제목변경(새글제목);
+            }}>글변경</span>
+            <span onClick={ () => { 따봉변경( 따봉 + 1 ) } }>👍</span>
+            { 따봉 }
+          </h4>
           <p>2월 17일 발행</p>
       </div>
       ))}
+
+      {
+        modal ? <Modal/> : null
+      }
+      
+
     </div>
   );
+}
+
+function Modal(){
+  return (
+    <div className='modal'>
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )  
 }
 
 export default App;
