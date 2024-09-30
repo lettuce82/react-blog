f23488b7d06f099d9d787b8b85dbe9a93bc6f5c1
ejddi1805 <82057874+ejddi1805@users.noreply.github.com>
2024-09-30 14:12:18 +0900

    Feat: 각 게시물별로 좋아요 수 올리기

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   let post = '강남 우동 맛집';
   let [글제목, 제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
-  let [따봉, 따봉변경]= useState(0);
+  let [따봉, 따봉변경]= useState([0,0,0]);
   let [modal, setModal] = useState(false);
 
   return (
@@ -23,20 +23,27 @@ function App() {
         }}>가나다순 정렬</button>
       </div>
 
-      {글제목.map((제목, index) => (
-        <div className='list' key={index}>
-          <h4 onClick={()=> {setModal(!modal)}}>{제목}
-            <span onClick={() => {
-            const 새글제목 = [...글제목];
-            새글제목[index] = '여자 코트 추천';
-            제목변경(새글제목);
-            }}>글변경</span>
-            <span onClick={ () => { 따봉변경( 따봉 + 1 ) } }>👍</span>
-            { 따봉 }
-          </h4>
-          <p>2월 17일 발행</p>
-      </div>
-      ))}
+      {
+        글제목.map((제목, index) => (
+          <div className='list' key={index}>
+            <h4 onClick={()=> {setModal(!modal)}}>{제목}</h4>
+            <span onClick={ () => { 
+
+              const 새따봉 = [...따봉];
+              새따봉[index]++;
+              따봉변경(새따봉);
+
+              } }>👍
+            </span> { 따봉[index] }
+            <button onClick={() => {
+              const 새글제목 = [...글제목];
+              새글제목[index] = '여자 코트 추천';
+              제목변경(새글제목);
+              }}>글변경</button>
+            <p>2월 17일 발행</p>
+        </div>
+        ))
+      }
 
       {
         modal ? <Modal/> : null
